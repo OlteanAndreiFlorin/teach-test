f173470302f7114bb5aac6e49cf1619ade0b3f7d
Oltean Andrei-Florin <oltean.andrei.florin@gmail.com>
2016-01-24 17:59:24 +0200

    Initial project commit.

---
+++ teach_and_learn/src/main/java/ro/sci/group2/web/IndexController.java
@@ -0,0 +1,13 @@
+package ro.sci.group2.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping("/")
+	String index() {
+		return "index";
+	}
+}
